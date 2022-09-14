b746e0874312e416cc09431b6c496acb03c15a6e
ZEHRAUZEKMEK <zehra.uzekmek@gmail.com>
2022-09-14 15:04:25 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Bu bir veritabanı nesnesidir.
+    //IEntity implement eden class bir database tablosudur.
+    public interface IEntity
+    {
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class Kalmasın. ÇCK
+    //Concrete klasöründeki classlar bir veritabanı tablosuna karşılık geliyor.
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
