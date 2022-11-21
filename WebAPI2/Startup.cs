d8db5c035de3cedee0f85b68506adaaa0af49b1a
ZEHRAUZEKMEK <zehra.uzekmek@gmail.com>
2022-11-21 13:45:37 +0300

    21.11.22

--- WebAPI2/Startup.cs
+++ WebAPI2/Startup.cs
@@ -40,7 +40,7 @@ namespace WebAPI2
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
-
+            services.AddCors();
             
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
@@ -71,6 +71,7 @@ namespace WebAPI2
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
             app.UseRouting();
